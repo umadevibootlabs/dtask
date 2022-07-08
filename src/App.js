009bdca198cc743c2a9522e33e92077af1470678
umadevibootlabs <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>
2022-07-08 20:50:28 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Navbar from "./components/layout/Navbar";
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
-  withRouter
+  Switch
 } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
-import AddUser from "./components/users/AddUser";
-import User from "./components/users/User";
-import EditUser from "./components/users/EditUser";
-
 function App(props) {
   return (
     <Router>
@@ -22,11 +16,7 @@ function App(props) {
         <Navbar />
 
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/users/add" component={AddUser} />
-          <Route exact path="/users/:id" component={User} />
-          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/" component={About} />
           <Route component={NotFound} />
         </Switch>
       </div>

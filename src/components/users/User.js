8e8330a95eeaca861bb07c13763c16679ee7033e
umadevibootlabs <99386847+umadevibootlabs@users.noreply.github.com>
2022-07-08 19:03:09 +0530

    Update User.js

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
-function User {
+function User() {
 //   const [user, setUser] = useState({
 //     name: "",
 //     department: "",
